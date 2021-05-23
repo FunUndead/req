fc3666d933a475446abb7bb7ae260d4a2f4ff362
Maxim Shihatarov <shix97@mail.ru>
2021-05-23 16:31:47 +0400

    NEW 23-05-21

--- req.py
+++ req.py
@@ -6,19 +6,24 @@ TOKEN = "2619421814940190"
 
 def iq(name):
     url = "https://superheroapi.com/api/"
+    dict_iq = {}
     link = "/search/"
     x = name
-    res = requests.get(url+TOKEN+link+x)
-    data = res.json()
-    #print(data['results'])
-    for i in data['results']:
-        if i['name'] == x:
-            iq = i['powerstats']['intelligence']
-    return int(iq)
+    for hero in x:
+        res = requests.get(url+TOKEN+link+hero)
+        data = res.json()
+        #print(data['results'])
+        for i in data['results']:
+            if i['name'] == hero:
+                iq = i['powerstats']['intelligence']
+                dict_iq[hero] = int(iq)
+    list_d = list(dict_iq.items())
+    list_d.sort(reverse= True)
 
-hulk = iq("Hulk")
-captain_america = iq("Captain America")
-thanos = iq("Thanos")
-#print (hulk, captain_america, thanos)
-print ('Самый умный герой Танос!')
+    return print(f"Самый умный герой {list_d[0][0]}, у него {list_d[0][1]}IQ !")
+
+list_hero = ["Hulk", "Captain America", "Thanos"]
+iq(list_hero)
+
+
 
